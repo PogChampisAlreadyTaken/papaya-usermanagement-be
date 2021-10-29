ded29ecb4d296fa7dc89de512673add6c46d4328
fhesselfeld <franziska.hesselfeld@extern.mmi-media.de>
2021-10-29 13:59:09 +0200

    Initial commit for quarkus project

---
+++ src/test/java/de/PogChampIsAlreadyTaken/Papaya/Webshop/NativeUsermanagementIT.java
@@ -0,0 +1,9 @@
+package de.PogChampIsAlreadyTaken.Papaya.Webshop;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeUsermanagementIT extends UsermanagementTest {
+
+    // Execute the same tests but in native mode.
+}
